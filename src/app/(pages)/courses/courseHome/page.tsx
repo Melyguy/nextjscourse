0b1731b5d7175c2d69ac46a75af8e6cda558e7c1
Odin-Johan <nidojohan@gmail.com>
2025-03-13 12:35:29 +0100

    im tweaking!

---
+++ src/app/(pages)/courses/courseHome/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/app/component/Footer";
+
+export default function courses() {
+    return(
+        <div className="w-screen h-96 grid grid-row gap-1 mt-32 justify-center items-center font-[family-name:var(--font-geist-mono)]">
+            <h1 className="text-6xl">Courses</h1>
+
+        
+            <h1 className="text-2xl">We have a variety of courses and tasks that are meant to be easy to understand for beginners. Take a look!   </h1>
+        
+        <Footer></Footer>
+        </div>
+
+    );
+}
